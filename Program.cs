23e4c3abf52374291ea54b327f3573a280918b7a
CuSO₄ · 5H₂O <5h2o@cuso4.tech>
2019-05-04 02:57:27 +0800

    Add codes

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace runau
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    ProcessExtensions.StartProcessAsCurrentUser(args[0]);
+                    break;
+                case 2:
+                    ProcessExtensions.StartProcessAsCurrentUser(args[0],args[1]);
+                    break;
+                case 3:
+                    ProcessExtensions.StartProcessAsCurrentUser(args[0],args[1],args[2]);
+                    break;
+                case 4:
+                    ProcessExtensions.StartProcessAsCurrentUser(args[0], args[1], args[2],(args[3]=="-h"));
+                    break;
+            }
+        }
+    }
+}
